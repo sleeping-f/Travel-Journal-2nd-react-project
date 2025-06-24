85fbe87663f36714dd95d4eedffcc54c348a66a5
Md. Farhan Sadique <mdfarhan.ngn@gmail.com>
2025-06-24 16:21:22 +0600

    Header section

---
+++ components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header className="HeaderLayoout">
+            <nav classname="NavbarHeader">
+                <img src="/src/assets/world-icon.png" alt="world icon" className="world-icon" />
+                <span className="HeaderTitile">my travel journal</span>
+            </nav>
+        </header>
+    )
+}
